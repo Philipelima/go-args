78501c476878286616943f0897e3f9a26a12651d
philipelima <philipematheus.lim@gmail.com>
2025-02-25 20:19:59 -0300

    feat: handle parser error

--- example/main.go
+++ example/main.go
@@ -2,23 +2,25 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/philipelima/go-args/pkg/args"
 )
 
 func main() {
 
-	ars := args.AsMap()
+	argMap := args.AsMap()
+	fmt.Println("\nArgs as a Map:", argMap)
 
-	fmt.Println("\nArgs as a Map: ", ars)
-
-	var User struct {
+	var user struct {
 		Name string `go_arg:"name"`
 		Age  int    `go_arg:"age"`
 	}
 
-	parser := args.NewParser(&User)
-	parser.Parse()
+	parser := args.NewParser(&user)
+	if err := parser.Parse(); err != nil {
+		log.Fatalf("Error parsing arguments: %v", err)
+	}
 
-	fmt.Println("\nArgs as a Struct: ", User)
+	fmt.Println("\nArgs as a Struct:", user)
 }
